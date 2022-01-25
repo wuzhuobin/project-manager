9ef7fe22802fc84d9e8cbccec40a2d93de9f7bf1
wuzhuobin <jiejin2022@163.com>
2022-01-25 07:58:28 -0800

    Add retrieve project fields function tmp

--- test/e2e/project.test.js
+++ test/e2e/project.test.js
@@ -3,12 +3,18 @@ const Project = require('../../lib/project');
 describe("project", () => {
 
 	const project = new Project(TOKEN);
+	const organization = "philips-internal";
+	const number = 93;
 
 	test("getProjectId", async () => {
-		const organization = "philips-internal";
-		const number = 93;
 
 		const id = await project.getProjectId(organization, number);
 		expect(id).toBe("PN_kwDOA20Mnc0z2Q");
 	});
+
+	test("getProjectFields", async () => {
+
+		const fields = await project.getProjectFields(organization, number);
+		console.log(fields);
+	})
 });
