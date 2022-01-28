ad6075f6be09c6e981ebb5d0bfbfc905d415efed
wuzhuobin <zhuobin.wu@philips.com>
2022-01-28 16:57:17 +0800

    Add project fields and porject items

--- test/e2e/project.test.js
+++ test/e2e/project.test.js
@@ -7,38 +7,29 @@ describe("e2e", () => {
     const project = new Project(organization, number, TOKEN);
 
     test("getProjectId", async () => {
-      const id = await project.getProjectId(organization, number);
+      const id = await project.getProjectId();
       expect(id).toBe("PN_kwDOA20Mnc0z2Q");
     });
 
     test("getProjectFieldCount", async () => {
-      const count = await project.getProjectFieldCount(organization, number);
+      const count = await project.getProjectFieldCount();
       expect(count).toBe(12);
     });
 
-    test("getProjectFieldName", async () => {
-      const names = await project.getProjectFieldName(organization, number);
-      expect(names).toEqual([
-        "Title",
-        "Assignees",
-        "Status",
-        "Labels",
-        "Repository",
-        "Milestone",
-        "Priority",
-        "Story Point",
-        "Comment",
-        "Sprint",
-        "Linked Pull Requests",
-        "Reviewers",
-      ]);
+    test("getProjectFields", async () => {
+      const fields = await project.getProjectFields();
+      console.log(fields);
     });
 
-	test("getProjectItemCount", async () => {
-		const count = await project.getProjectItemCount(organization, number);
-		expect(count).toBe(104);
-	})
+    test("getProjectItemCount", async () => {
+      const count = await project.getProjectItemCount();
+      expect(count).toBe(104);
+    });
 
-
+    test("getProjectItems", async () => {
+      jest.setTimeout(10000);
+      const items = await project.getProjectItems();
+      expect(items.length).toEqual(104);
+    });
   });
 });
