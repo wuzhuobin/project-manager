a5d43438beaa16f1c5fb3149a2308ec1a2ae5989
wuzhuobin <zhuobin.wu@philips.com>
2022-02-08 17:48:26 +0800

    Fix the unittest for Render

--- test/unittest/render.test.js
+++ test/unittest/render.test.js
@@ -1,43 +1,179 @@
 const Render = require("../../lib/render");
 
 describe("Render", () => {
-  test.skip("projectItemsByStatus", () => {
+  test("projectItemsByStatus", () => {
     const testStatusGroup = {
       98236657: {
         name: "🏁Done🏁",
         items: [
-          "PNI_lADOA20Mnc0z2c4AFIkf",
-          "PNI_lADOA20Mnc0z2c4AFQWj",
-          "PNI_lADOA20Mnc0z2c4AFYIT",
-          "PNI_lADOA20Mnc0z2c4AFYIX",
-          "PNI_lADOA20Mnc0z2c4AFt0H",
-          "PNI_lADOA20Mnc0z2c4AFz1V",
+          {
+            id: "PNI_lADOA20Mnc0z2c4AA9kB",
+            title: "What is ECDSA for ECC keys?",
+            content: {
+              __typename: "Issue",
+              id: "I_kwDOGPV02s49YTyQ",
+              number: 1,
+              url: "https://github.com/philips-internal/libscep/issues/1",
+            },
+            fieldValues: [
+              {
+                value: "Summary of the first phrase",
+                projectField: {
+                  id: "MDE2OlByb2plY3ROZXh0RmllbGQ5NTk0OA==",
+                  name: "Title",
+                },
+              },
+              {
+                value: "85617f5a",
+                projectField: {
+                  id: "MDE2OlByb2plY3ROZXh0RmllbGQ5NTk1MA==",
+                  name: "Status",
+                },
+              },
+              {
+                value: "e1a62df9",
+                projectField: {
+                  id: "MDE2OlByb2plY3ROZXh0RmllbGQxODc5NDM=",
+                  name: "Priority",
+                },
+              },
+              {
+                value: "0b16fa06",
+                projectField: {
+                  id: "MDE2OlByb2plY3ROZXh0RmllbGQyMzcyMDE=",
+                  name: "Sprint",
+                },
+              },
+            ],
+          },
+          {
+            id: "PNI_lADOA20Mnc0z2c4ABD7t",
+            title: "What is CMS(older version is PKCS #7)?",
+            content: {
+              __typename: "Issue",
+              id: "I_kwDOGPV02s49lDf-",
+              number: 2,
+              url: "https://github.com/philips-internal/libscep/issues/2",
+            },
+            fieldValues: [
+              {
+                value: "Summary of the first phrase",
+                projectField: {
+                  id: "MDE2OlByb2plY3ROZXh0RmllbGQ5NTk0OA==",
+                  name: "Title",
+                },
+              },
+              {
+                value: "85617f5a",
+                projectField: {
+                  id: "MDE2OlByb2plY3ROZXh0RmllbGQ5NTk1MA==",
+                  name: "Status",
+                },
+              },
+              {
+                value: "e1a62df9",
+                projectField: {
+                  id: "MDE2OlByb2plY3ROZXh0RmllbGQxODc5NDM=",
+                  name: "Priority",
+                },
+              },
+              {
+                value: "0b16fa06",
+                projectField: {
+                  id: "MDE2OlByb2plY3ROZXh0RmllbGQyMzcyMDE=",
+                  name: "Sprint",
+                },
+              },
+            ],
+          },
+          {
+            id: "PNI_lADOA20Mnc0z2c4ABMVU",
+            title: "What is PKCS #10 ?",
+            content: {
+              __typename: "Issue",
+              id: "I_kwDOGPV02s49zaRo",
+              number: 3,
+              url: "https://github.com/philips-internal/libscep/issues/3",
+            },
+            fieldValues: [
+              {
+                value: "Summary of the first phrase",
+                projectField: {
+                  id: "MDE2OlByb2plY3ROZXh0RmllbGQ5NTk0OA==",
+                  name: "Title",
+                },
+              },
+              {
+                value: "85617f5a",
+                projectField: {
+                  id: "MDE2OlByb2plY3ROZXh0RmllbGQ5NTk1MA==",
+                  name: "Status",
+                },
+              },
+              {
+                value: "e1a62df9",
+                projectField: {
+                  id: "MDE2OlByb2plY3ROZXh0RmllbGQxODc5NDM=",
+                  name: "Priority",
+                },
+              },
+              {
+                value: "0b16fa06",
+                projectField: {
+                  id: "MDE2OlByb2plY3ROZXh0RmllbGQyMzcyMDE=",
+                  name: "Sprint",
+                },
+              },
+            ],
+          },
         ],
       },
       "85617f5a": {
         name: ":hammer:Others:hammer:",
-        items: ["PNI_lADOA20Mnc0z2c4ACZsm", "PNI_lADOA20Mnc0z2c4AFtBg"],
+        items: [
+          {
+            id: "PNI_lADOA20Mnc0z2c4ABMVZ",
+            title: "How is the workflow of SCEP client and server?",
+            content: {
+              __typename: "Issue",
+              id: "I_kwDOGPV02s49zaR6",
+              number: 4,
+              url: "https://github.com/philips-internal/libscep/issues/4",
+            },
+            fieldValues: [],
+          },
+          {
+            id: "PNI_lADOA20Mnc0z2c4ABMVc",
+            title: "How is the the http transaction of SCEP?",
+            content: {
+              __typename: "Issue",
+              id: "I_kwDOGPV02s49zaSS",
+              number: 5,
+              url: "https://github.com/philips-internal/libscep/issues/5",
+            },
+            fieldValues: [],
+          },
+          {
+            id: "PNI_lADOA20Mnc0z2c4ABMy9",
+            title:
+              "How to deploy the openxpki server or IX-certificate in docker environment",
+            content: {
+              __typename: "Issue",
+              id: "I_kwDOGPV02s493sgl",
+              number: 6,
+              url: "https://github.com/philips-internal/libscep/issues/6",
+            },
+            fieldValues: [],
+          },
+        ],
       },
       "9c41f080": {
         name: ":scroll:Backlog:scroll:",
-        items: [
-          "PNI_lADOA20Mnc0z2c4AByni",
-          "PNI_lADOA20Mnc0z2c4ABzyI",
-          "PNI_lADOA20Mnc0z2c4AB3ol",
-        ],
+        items: [],
       },
       f75ad846: {
         name: "🚩Todo🚩",
-        items: [
-          "PNI_lADOA20Mnc0z2c4AES6Z",
-          "PNI_lADOA20Mnc0z2c4AEpPF",
-          "PNI_lADOA20Mnc0z2c4AEpqD",
-          "PNI_lADOA20Mnc0z2c4AEprf",
-          "PNI_lADOA20Mnc0z2c4AEpr0",
-          "PNI_lADOA20Mnc0z2c4AEwNi",
-          "PNI_lADOA20Mnc0z2c4AE2_H",
-          "PNI_lADOA20Mnc0z2c4AFBIu",
-        ],
+        items: [],
       },
       "47fc9ee4": {
         name: "🐝In Progress🐝",
