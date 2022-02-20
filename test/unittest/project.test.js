a829f8964089b5daf96bd36fea5994ca9eea55b8
wuzhuobin <jiejin2022@163.com>
2022-02-20 08:31:56 -0800

    Fix tests which modifed test data

--- test/unittest/project.test.js
+++ test/unittest/project.test.js
@@ -3,172 +3,183 @@ jest.mock("axios");
 const axios = require("axios").default;
 
 describe("Project", () => {
-  const organization = "philips-internal";
-  const number = 93;
-  const testItemId = "PNI_lADOA20Mnc0z2c4AFtBg";
-  const testItemFieldValues = [
-    {
-      value: "Summary of the first phrase",
-      projectField: {
-        id: "MDE2OlByb2plY3ROZXh0RmllbGQ5NTk0OA==",
-        name: "Title",
+  let organization;
+  let number;
+  let testItemId;
+  let testItemFieldValues;
+  let testFields;
+  let testItems;
+  let testAssigness;
+  let project;
+
+  beforeEach(() => {
+    organization = "philips-internal";
+    number = 93;
+    testItemId = "PNI_lADOA20Mnc0z2c4AFtBg";
+    testItemFieldValues = [
+      {
+        value: "Summary of the first phrase",
+        projectField: {
+          id: "MDE2OlByb2plY3ROZXh0RmllbGQ5NTk0OA==",
+          name: "Title",
+        },
       },
-    },
-    {
-      value: "85617f5a",
-      projectField: {
+      {
+        value: "85617f5a",
+        projectField: {
+          id: "MDE2OlByb2plY3ROZXh0RmllbGQ5NTk1MA==",
+          name: "Status",
+        },
+      },
+      {
+        value: "e1a62df9",
+        projectField: {
+          id: "MDE2OlByb2plY3ROZXh0RmllbGQxODc5NDM=",
+          name: "Priority",
+        },
+      },
+      {
+        value: "0b16fa06",
+        projectField: {
+          id: "MDE2OlByb2plY3ROZXh0RmllbGQyMzcyMDE=",
+          name: "Sprint",
+        },
+      },
+    ];
+    testFields = [
+      {
         id: "MDE2OlByb2plY3ROZXh0RmllbGQ5NTk1MA==",
         name: "Status",
+        settings: JSON.parse(
+          '{"width":153,"options":[{"id":"85617f5a","name":":hammer:Others:hammer:","name_html":"<g-emoji class=\\"g-emoji\\" alias=\\"hammer\\" fallback-src=\\"https://github.githubassets.com/images/icons/emoji/unicode/1f528.png\\">🔨</g-emoji>Others<g-emoji class=\\"g-emoji\\" alias=\\"hammer\\" fallback-src=\\"https://github.githubassets.com/images/icons/emoji/unicode/1f528.png\\">🔨</g-emoji>"},{"id":"9c41f080","name":":scroll:Backlog:scroll:","name_html":"<g-emoji class=\\"g-emoji\\" alias=\\"scroll\\" fallback-src=\\"https://github.githubassets.com/images/icons/emoji/unicode/1f4dc.png\\">📜</g-emoji>Backlog<g-emoji class=\\"g-emoji\\" alias=\\"scroll\\" fallback-src=\\"https://github.githubassets.com/images/icons/emoji/unicode/1f4dc.png\\">📜</g-emoji>"},{"id":"f75ad846","name":"🚩Todo🚩","name_html":"<g-emoji class=\\"g-emoji\\" alias=\\"triangular_flag_on_post\\" fallback-src=\\"https://github.githubassets.com/images/icons/emoji/unicode/1f6a9.png\\">🚩</g-emoji>Todo<g-emoji class=\\"g-emoji\\" alias=\\"triangular_flag_on_post\\" fallback-src=\\"https://github.githubassets.com/images/icons/emoji/unicode/1f6a9.png\\">🚩</g-emoji>"},{"id":"47fc9ee4","name":"🐝In Progress🐝","name_html":"<g-emoji class=\\"g-emoji\\" alias=\\"bee\\" fallback-src=\\"https://github.githubassets.com/images/icons/emoji/unicode/1f41d.png\\">🐝</g-emoji>In Progress<g-emoji class=\\"g-emoji\\" alias=\\"bee\\" fallback-src=\\"https://github.githubassets.com/images/icons/emoji/unicode/1f41d.png\\">🐝</g-emoji>"},{"id":"98236657","name":"🏁Done🏁","name_html":"<g-emoji class=\\"g-emoji\\" alias=\\"checkered_flag\\" fallback-src=\\"https://github.githubassets.com/images/icons/emoji/unicode/1f3c1.png\\">🏁</g-emoji>Done<g-emoji class=\\"g-emoji\\" alias=\\"checkered_flag\\" fallback-src=\\"https://github.githubassets.com/images/icons/emoji/unicode/1f3c1.png\\">🏁</g-emoji>"}]}'
+        ),
       },
-    },
-    {
-      value: "e1a62df9",
-      projectField: {
+      {
+        id: "MDE2OlByb2plY3ROZXh0RmllbGQ5NTk1MQ==",
+        name: "Labels",
+        settings: JSON.parse('{"width":288}'),
+      },
+      {
         id: "MDE2OlByb2plY3ROZXh0RmllbGQxODc5NDM=",
         name: "Priority",
+        settings: JSON.parse(
+          '{"width":139,"options":[{"id":"d025e06c","name":"❗High❗","name_html":"<g-emoji class=\\"g-emoji\\" alias=\\"exclamation\\" fallback-src=\\"https://github.githubassets.com/images/icons/emoji/unicode/2757.png\\">❗</g-emoji>High<g-emoji class=\\"g-emoji\\" alias=\\"exclamation\\" fallback-src=\\"https://github.githubassets.com/images/icons/emoji/unicode/2757.png\\">❗</g-emoji>"},{"id":"e056cb71","name":"❕Medium❕","name_html":"<g-emoji class=\\"g-emoji\\" alias=\\"grey_exclamation\\" fallback-src=\\"https://github.githubassets.com/images/icons/emoji/unicode/2755.png\\">❕</g-emoji>Medium<g-emoji class=\\"g-emoji\\" alias=\\"grey_exclamation\\" fallback-src=\\"https://github.githubassets.com/images/icons/emoji/unicode/2755.png\\">❕</g-emoji>"},{"id":"5e1391e6","name":"❓Low❓","name_html":"<g-emoji class=\\"g-emoji\\" alias=\\"question\\" fallback-src=\\"https://github.githubassets.com/images/icons/emoji/unicode/2753.png\\">❓</g-emoji>Low<g-emoji class=\\"g-emoji\\" alias=\\"question\\" fallback-src=\\"https://github.githubassets.com/images/icons/emoji/unicode/2753.png\\">❓</g-emoji>"},{"id":"e1a62df9","name":"📍None📍","name_html":"<g-emoji class=\\"g-emoji\\" alias=\\"round_pushpin\\" fallback-src=\\"https://github.githubassets.com/images/icons/emoji/unicode/1f4cd.png\\">📍</g-emoji>None<g-emoji class=\\"g-emoji\\" alias=\\"round_pushpin\\" fallback-src=\\"https://github.githubassets.com/images/icons/emoji/unicode/1f4cd.png\\">📍</g-emoji>"}]}'
+        ),
       },
-    },
-    {
-      value: "0b16fa06",
-      projectField: {
+      {
+        id: "MDE2OlByb2plY3ROZXh0RmllbGQxODgwMTM=",
+        name: "Story Point",
+        settings: JSON.parse('{"width":119}'),
+      },
+      {
         id: "MDE2OlByb2plY3ROZXh0RmllbGQyMzcyMDE=",
         name: "Sprint",
-      },
-    },
-  ];
-  const testFields = [
-    {
-      id: "MDE2OlByb2plY3ROZXh0RmllbGQ5NTk1MA==",
-      name: "Status",
-      settings: JSON.parse(
-        '{"width":153,"options":[{"id":"85617f5a","name":":hammer:Others:hammer:","name_html":"<g-emoji class=\\"g-emoji\\" alias=\\"hammer\\" fallback-src=\\"https://github.githubassets.com/images/icons/emoji/unicode/1f528.png\\">🔨</g-emoji>Others<g-emoji class=\\"g-emoji\\" alias=\\"hammer\\" fallback-src=\\"https://github.githubassets.com/images/icons/emoji/unicode/1f528.png\\">🔨</g-emoji>"},{"id":"9c41f080","name":":scroll:Backlog:scroll:","name_html":"<g-emoji class=\\"g-emoji\\" alias=\\"scroll\\" fallback-src=\\"https://github.githubassets.com/images/icons/emoji/unicode/1f4dc.png\\">📜</g-emoji>Backlog<g-emoji class=\\"g-emoji\\" alias=\\"scroll\\" fallback-src=\\"https://github.githubassets.com/images/icons/emoji/unicode/1f4dc.png\\">📜</g-emoji>"},{"id":"f75ad846","name":"🚩Todo🚩","name_html":"<g-emoji class=\\"g-emoji\\" alias=\\"triangular_flag_on_post\\" fallback-src=\\"https://github.githubassets.com/images/icons/emoji/unicode/1f6a9.png\\">🚩</g-emoji>Todo<g-emoji class=\\"g-emoji\\" alias=\\"triangular_flag_on_post\\" fallback-src=\\"https://github.githubassets.com/images/icons/emoji/unicode/1f6a9.png\\">🚩</g-emoji>"},{"id":"47fc9ee4","name":"🐝In Progress🐝","name_html":"<g-emoji class=\\"g-emoji\\" alias=\\"bee\\" fallback-src=\\"https://github.githubassets.com/images/icons/emoji/unicode/1f41d.png\\">🐝</g-emoji>In Progress<g-emoji class=\\"g-emoji\\" alias=\\"bee\\" fallback-src=\\"https://github.githubassets.com/images/icons/emoji/unicode/1f41d.png\\">🐝</g-emoji>"},{"id":"98236657","name":"🏁Done🏁","name_html":"<g-emoji class=\\"g-emoji\\" alias=\\"checkered_flag\\" fallback-src=\\"https://github.githubassets.com/images/icons/emoji/unicode/1f3c1.png\\">🏁</g-emoji>Done<g-emoji class=\\"g-emoji\\" alias=\\"checkered_flag\\" fallback-src=\\"https://github.githubassets.com/images/icons/emoji/unicode/1f3c1.png\\">🏁</g-emoji>"}]}'
-      ),
-    },
-    {
-      id: "MDE2OlByb2plY3ROZXh0RmllbGQ5NTk1MQ==",
-      name: "Labels",
-      settings: JSON.parse('{"width":288}'),
-    },
-    {
-      id: "MDE2OlByb2plY3ROZXh0RmllbGQxODc5NDM=",
-      name: "Priority",
-      settings: JSON.parse(
-        '{"width":139,"options":[{"id":"d025e06c","name":"❗High❗","name_html":"<g-emoji class=\\"g-emoji\\" alias=\\"exclamation\\" fallback-src=\\"https://github.githubassets.com/images/icons/emoji/unicode/2757.png\\">❗</g-emoji>High<g-emoji class=\\"g-emoji\\" alias=\\"exclamation\\" fallback-src=\\"https://github.githubassets.com/images/icons/emoji/unicode/2757.png\\">❗</g-emoji>"},{"id":"e056cb71","name":"❕Medium❕","name_html":"<g-emoji class=\\"g-emoji\\" alias=\\"grey_exclamation\\" fallback-src=\\"https://github.githubassets.com/images/icons/emoji/unicode/2755.png\\">❕</g-emoji>Medium<g-emoji class=\\"g-emoji\\" alias=\\"grey_exclamation\\" fallback-src=\\"https://github.githubassets.com/images/icons/emoji/unicode/2755.png\\">❕</g-emoji>"},{"id":"5e1391e6","name":"❓Low❓","name_html":"<g-emoji class=\\"g-emoji\\" alias=\\"question\\" fallback-src=\\"https://github.githubassets.com/images/icons/emoji/unicode/2753.png\\">❓</g-emoji>Low<g-emoji class=\\"g-emoji\\" alias=\\"question\\" fallback-src=\\"https://github.githubassets.com/images/icons/emoji/unicode/2753.png\\">❓</g-emoji>"},{"id":"e1a62df9","name":"📍None📍","name_html":"<g-emoji class=\\"g-emoji\\" alias=\\"round_pushpin\\" fallback-src=\\"https://github.githubassets.com/images/icons/emoji/unicode/1f4cd.png\\">📍</g-emoji>None<g-emoji class=\\"g-emoji\\" alias=\\"round_pushpin\\" fallback-src=\\"https://github.githubassets.com/images/icons/emoji/unicode/1f4cd.png\\">📍</g-emoji>"}]}'
-      ),
-    },
-    {
-      id: "MDE2OlByb2plY3ROZXh0RmllbGQxODgwMTM=",
-      name: "Story Point",
-      settings: JSON.parse('{"width":119}'),
-    },
-    {
-      id: "MDE2OlByb2plY3ROZXh0RmllbGQyMzcyMDE=",
-      name: "Sprint",
-      settings: JSON.parse(
-        '{"width":156,"configuration":{"duration":20,"start_day":2,"iterations":[],"completed_iterations":[{"id":"39857db5","title":"Sprint 3","duration":20,"start_date":"2022-01-01","title_html":"Sprint 3"},{"id":"0b16fa06","title":"Sprint 2","duration":16,"start_date":"2021-12-16","title_html":"Sprint 2"},{"id":"48a795a4","title":"Sprint 1","duration":50,"start_date":"2021-10-27","title_html":"Sprint 1"}]}}'
-      ),
-    },
-    {
-      id: "MDE2OlByb2plY3ROZXh0RmllbGQ0MTg2OTg=",
-      name: "Linked Pull Requests",
-      settings: JSON.parse("null"),
-    },
-  ];
-  const testItems = [
-    {
-      id: "PNI_lADOA20Mnc0z2c4AByoE",
-      title: "Providing apis for generating CSR",
-      content: {
-        __typename: "Issue",
-        id: "I_kwDOGPV02s4-eq_M",
-        number: 12,
-        url: "https://github.com/philips-internal/libscep/issues/12",
-      },
-    },
-    {
-      id: "PNI_lADOA20Mnc0z2c4ABD7t",
-      title: "What is CMS(older version is PKCS #7)?",
-      content: {
-        __typename: "Issue",
-        id: "I_kwDOGPV02s49lDf-",
-        number: 2,
-        url: "https://github.com/philips-internal/libscep/issues/2",
-      },
-    },
-    {
-      id: "PNI_lADOA20Mnc0z2c4AB5ll",
-      title: "Implement renew functions in scep",
-      content: {
-        __typename: "Issue",
-        id: "I_kwDOGPV02s4-nM2-",
-        number: 21,
-        url: "https://github.com/philips-internal/libscep/issues/21",
-      },
-    },
-    {
-      id: "PNI_lADOA20Mnc0z2c4AB4FM",
-      title: "Figure out a way to build the SecMgr easily",
-      content: {
-        __typename: "Issue",
-        id: "I_kwDOGPV02s4-lfyG",
-        number: 20,
-        url: "https://github.com/philips-internal/libscep/issues/20",
-      },
-    },
-    {
-      id: "PNI_lADOA20Mnc0z2c4AB5o5",
-      title:
-        "component tests for demonstrating the GetCA->Enrolment->Renewal is working",
-      content: {
-        __typename: "Issue",
-        id: "I_kwDOGPV02s4-nSEY",
-        number: 24,
-        url: "https://github.com/philips-internal/libscep/issues/24",
-      },
-    },
-  ];
-  const testAssigness = [
-    [],
-    [
-      {
-        name: "Ares.Zhao",
-        login: "AresZhaoLei",
+        settings: JSON.parse(
+          '{"width":156,"configuration":{"duration":20,"start_day":2,"iterations":[],"completed_iterations":[{"id":"39857db5","title":"Sprint 3","duration":20,"start_date":"2022-01-01","title_html":"Sprint 3"},{"id":"0b16fa06","title":"Sprint 2","duration":16,"start_date":"2021-12-16","title_html":"Sprint 2"},{"id":"48a795a4","title":"Sprint 1","duration":50,"start_date":"2021-10-27","title_html":"Sprint 1"}]}}'
+        ),
       },
       {
-        name: "Linus Deng",
-        login: "amelonpie",
+        id: "MDE2OlByb2plY3ROZXh0RmllbGQ0MTg2OTg=",
+        name: "Linked Pull Requests",
+        settings: JSON.parse("null"),
       },
-    ],
-    [
+    ];
+    testItems = [
       {
-        name: "wuzhuobin",
-        login: "wuzhuobin",
-      },
-      {
-        name: null,
-        login: "ruixiaoli",
-      },
-    ],
-    [
-      {
-        name: "He Quan Liang",
-        login: "Stevenson-1320",
+        id: "PNI_lADOA20Mnc0z2c4AByoE",
+        title: "Providing apis for generating CSR",
+        content: {
+          __typename: "Issue",
+          id: "I_kwDOGPV02s4-eq_M",
+          number: 12,
+          url: "https://github.com/philips-internal/libscep/issues/12",
+        },
       },
       {
-        name: null,
-        login: "NickNingCH",
+        id: "PNI_lADOA20Mnc0z2c4ABD7t",
+        title: "What is CMS(older version is PKCS #7)?",
+        content: {
+          __typename: "Issue",
+          id: "I_kwDOGPV02s49lDf-",
+          number: 2,
+          url: "https://github.com/philips-internal/libscep/issues/2",
+        },
       },
-    ],
-    [
       {
-        name: "wuzhuobin",
-        login: "wuzhuobin",
+        id: "PNI_lADOA20Mnc0z2c4AB5ll",
+        title: "Implement renew functions in scep",
+        content: {
+          __typename: "Issue",
+          id: "I_kwDOGPV02s4-nM2-",
+          number: 21,
+          url: "https://github.com/philips-internal/libscep/issues/21",
+        },
       },
-    ],
-  ];
-  const project = new Project(null, null, true);
-  project.setOrignization(organization);
-  project.setProjectNumber(number);
+      {
+        id: "PNI_lADOA20Mnc0z2c4AB4FM",
+        title: "Figure out a way to build the SecMgr easily",
+        content: {
+          __typename: "Issue",
+          id: "I_kwDOGPV02s4-lfyG",
+          number: 20,
+          url: "https://github.com/philips-internal/libscep/issues/20",
+        },
+      },
+      {
+        id: "PNI_lADOA20Mnc0z2c4AB5o5",
+        title:
+          "component tests for demonstrating the GetCA->Enrolment->Renewal is working",
+        content: {
+          __typename: "Issue",
+          id: "I_kwDOGPV02s4-nSEY",
+          number: 24,
+          url: "https://github.com/philips-internal/libscep/issues/24",
+        },
+      },
+    ];
+    testAssigness = [
+      [],
+      [
+        {
+          name: "Ares.Zhao",
+          login: "AresZhaoLei",
+        },
+        {
+          name: "Linus Deng",
+          login: "amelonpie",
+        },
+      ],
+      [
+        {
+          name: "wuzhuobin",
+          login: "wuzhuobin",
+        },
+        {
+          name: null,
+          login: "ruixiaoli",
+        },
+      ],
+      [
+        {
+          name: "He Quan Liang",
+          login: "Stevenson-1320",
+        },
+        {
+          name: null,
+          login: "NickNingCH",
+        },
+      ],
+      [
+        {
+          name: "wuzhuobin",
+          login: "wuzhuobin",
+        },
+      ],
+    ];
+    project = new Project(null, null, true);
+    project.setOrignization(organization);
+    project.setProjectNumber(number);
+  });
 
   test("getProjectId", async () => {
     axios.post.mockResolvedValue({
